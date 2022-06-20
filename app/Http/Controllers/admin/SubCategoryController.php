62ed66424f10fdda07372b24bad1875ac1617116
unknown <donia.shaker11@gmail.com>
2022-06-20 12:32:01 +0300

    adminManageCategoriesAndSubCategories

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -9,18 +9,198 @@ use Illuminate\Http\Request;
 
 class SubCategoryController extends Controller
 {
+    public function showSubCategory($id){
+        $subCategory = categories::select()->where('parentId',$id)->get();
+        // return $subCategory;
+        return view('admin.subCategory')->with([
+            'subCategories' => $subCategory ,
+            'locale'    => app()->getLocale()
+        ]);
+    }
+
+    function fetchSubCategory($id){
+        $subCategory = categories::select()->where('parentId',$id)->get();
+        // $categories = categories::select()->where('parentId', '0')->get();
+        return response()->json([
+            'categories' => $subCategory ,
+            'locale'    => app()->getLocale()
+        ]);
+    }
+
+    function addSubCategory(Request $request){
+        // return $request;
+        $validator = Validator::make($request->all(),[
+            'nameEn'=>['required','string', 'between: 3, 30', 'unique:categories,name'],
+            'nameAr'=>['required','string', 'between: 3, 30', 'unique:categories,name'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->getMessageBag(),
+            ]);
+        } else{
+            $category=new categories;
+            $data =  ['en' => $request->input('nameEn'),'ar' => $request->input('nameAr')];
+            $category->name = json_encode($data) ;
+            $category->parentId = $request->input('parentId') ;
+
+            if($request->input('active') != null){
+                $category->is_active=1;
+            }
+            // return $category;
+
+            if($category->save())
+            
+            return response()->json(
+                [
+                // 'category'=> $category,
+                'status' => 200,
+                'message' => 'Data inserted successfully',
+                
+                ]
+            );
+            // return $category->name;
+        }
+    }
+
+    public function editSubCategory( $id){
+        $category = categories::find($id);
+        if($category){
+            return response()->json(
+                [
+                // 'category'=> $category,
+                'status' => 200,
+                'category' => $category,
+                
+                ]
+            );
+        }else {
+            return response()->json(
+                [
+                // 'category'=> $category,
+                'status' => 404,
+                'message' =>'Data not Found',
+                
+                ]
+            );
+        }
+
+    }
+
+    public function updateSubCategory(Request $request , $id){
+        //  return $request;
+         $validator = Validator::make($request->all(),[
+            'nameEn'=>['required','string', 'between: 3, 30', 'unique:categories,name'],
+            'nameAr'=>['required','string', 'between: 3, 30', 'unique:categories,name'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->getMessageBag(),
+            ]);
+        } else{
+            $category= categories::find($id);
+
+            if($category){
+                $data =  ['en' => $request->input('nameEn'),'ar' => $request->input('nameAr')];
+                $category->name = json_encode($data) ;
+                $category->parentId = $request->input('parentId') ;
+    
+                if($request->input('active') != null){
+                    $category->is_active=1;
+                }
+                // return $category;
+    
+                if($category->update()){
+                
+                    return response()->json(
+                        [
+                        // 'category'=> $category,
+                        'status' => 200,
+                        'message' => 'Data Update successfully',
+                        
+                        ]
+                    );
+                // return $category->name;
+            }else {
+                return response()->json(
+                    [
+                    'status' => 404,
+                    'message' =>'Data not Found',
+                    
+                    ]
+                );
+                }
+            }
+        }
+    }
+
+    public function activeSubCategory($id){
+        $category = categories::find($id);
+
+        if($category->is_active )
+            $category->is_active=0;
+        else 
+            $category->is_active=1;
+        if($category->save()){
+            return response()->json(
+                [
+                'active'=> $category,
+                'status' => 200,
+                'message' => 'Data Update successfully',
+                    
+                    ]
+                );
+            // return $category->name;
+            }else {
+                return response()->json(
+                    [
+                    'status' => 404,
+                    'message' =>'Data not Found',
+                    
+                    ]
+                );
+                }
+        }
+
+        public function deleteSubCategory($id){
+            $category = categories::find($id);
+            if($category->delete()){
+                return response()->json(
+                [
+                'active'=> $category,
+                'status' => 200,
+                'message' => 'Data Delete successfully',
+                    
+                    ]
+                );
+            // return $category->name;
+            }else {
+                return response()->json(
+                    [
+                    'status' => 404,
+                    'message' =>'Data not Found',
+                    
+                    ]
+                );
+                }
+        }
+    }
+
     // function showSubCategory(){
     //     return view('admin.subCategory');
     // }
 
-    public function showSubCategory($id){
+    // public function showSubCategory($id){
         // $subCategory = categories::select()->where('parentId',$id)->get();
-        return view('admin.subCategory');
+        // return view('admin.subCategory');
         // return response()->json([
         //     'categories' => $subCategory ,
         //     'locale'    => app()->getLocale()
         // ]);
-    }
+    // }
 
 
-}
+// }
--- resources/views/admin/subCategory.blade.php
+++ resources/views/admin/subCategory.blade.php
@@ -1,141 +1,184 @@
 @extends('admin.layout.dashboard')
 @section('content')
-<!-- Basic Bootstrap Table -->
-<div class="" role="alert" id="message">
-</div>
-<div class="card">
-    <h1 name="tableName">Table Basic</h1>
-    <button type="button" class="btn btn-primary m-4 w-25"
-    data-bs-toggle="modal" data-bs-target="#categoryModal" id="addCategory"> اضافه خدمه </button>
-    <x-table>
-      <div class="table-responsive text-nowrap">
-        <table class="table">
-          <x-slot name="tableHead">
-            <tr>
-              <th>#</th>
-              <th>(ar)الخدمات الرئيسيه</th>
-              <th>(en)الخدمات الرئيسيه</th>
-              <th>الخدمات الفرعيه</th>
-              <th>الحاله</th>
-              <th>العمليات</th>
-            </tr>
-          </x-slot>
-          <x-slot name="tableBody" id="tbody" >
-            
-          </x-slot>
-        </table>
-      </div>
-    </x-table>
-</div>
-  <!--/ Basic Bootstrap Table -->
-  <!-- Add New Credit Card Modal -->
-<div class="modal fade" id="categoryModal" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
-    <div class="modal-content p-3 p-md-5">
-      <div class="modal-body">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        <div class="text-center mb-4">
-          <h4 class="modal-title" id="userCrudModal"></h4>
-          <p>اضف خدمة جديدة للموقع</p>
-        </div>
-        <form id="addCategoryForm" class="row g-3" action="addCategory" method="POST" enctype="multipart/form-data">
-          @csrf
-            <div>
-              <label for="defaultFormControlInput" class="form-label">  الاسم (عربي)</label>
-              <input type="text" name="name[ar]" class="form-control" id="name.ar" placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
-              <span class="help-block"><strong></strong></span>
-            </div>
-            <div>
-              <label for="defaultFormControlInput" class="form-label">الاسم (انجليزي)</label>
-              <input type="text" name="name[en]" class="form-control" id="name.en" placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
-              <span class="help-block"><strong></strong></span>
-            </div>
-          <div class="col-12 my-4">
-            <label class="switch">
-              <input type="checkbox" class="switch-input" name="active" id="active" value="1">
-              <span class="switch-toggle-slider">
-                <span class="switch-on"></span>
-                <span class="switch-off"></span>
-              </span>
-              <span class="switch-label"  >تفعيل </span>
-            </label>
-          </div>
-          <div class="col-12 text-center">
-            <button type="submit" id="submit" class="btn btn-primary me-sm-3 me-1 mt-3">اضافة</button>
-            <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
-          </div>
-        </form>
-      </div>
+    <!-- Basic Bootstrap Table -->
+    <div class="" role="alert" id="message">
     </div>
-  </div>
-</div>
-<!--/ Add New Credit Card Modal -->
+    <div class="card p-3">
+        <h2 name="tableName m-5">الخدمات الرئيسية</h2>
+        <button type="button" class="btn btn-primary m-4 w-25" data-bs-toggle="modal" data-bs-target="#addSubCategoryModal"
+            id="addSubCategory"> اضافه خدمه </button>
+        <x-table>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <x-slot name="tableHead">
+                        <tr>
+                            <th>#</th>
+                            <th>(ar)اسم التصنيف</th>
+                            <th>(en)اسم التصنيف</th>
+                            <th>الحاله</th>
+                            <th>العمليات</th>
+                        </tr>
+                    </x-slot>
+                    <x-slot name="tableBody" id="tbody">
+                    </x-slot>
+                </table>
+            </div>
+        </x-table>
+    </div>
+    <!--/ Basic Bootstrap Table -->
 
-  <!-- Updat New Credit Card Modal -->
-  <div class="modal fade" id="editCategoryModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
-      <div class="modal-content p-3 p-md-5">
-        <div class="modal-body">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          <div class="text-center mb-4">
-            <h4 class="modal-title" id="userCrudModal"></h4>
-            <p>تعديل الخدمة  </p>
-          </div>
-          <form id="updateCategory" class="row g-3" action="updateCategory" method="POST" enctype="multipart/form-data">
-            @csrf
-            <input type="hidden" id="editCategoryId" >
-              <div>
-                <label for="defaultFormControlInput" class="form-label">  الاسم (عربي)</label>
-                <input type="text" name="nameAr" class="form-control" id="editNameAr" placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
-                <span class="help-block"><strong></strong></span>
-              </div>
-              <div>
-                <label for="defaultFormControlInput" class="form-label">الاسم (انجليزي)</label>
-                <input type="text" name="nameEn" class="form-control" id="editNameEn" placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
-                <span class="help-block"><strong></strong></span>
-              </div>
-            <div class="col-12 my-4">
-              <label class="switch">
-                <input type="checkbox" class="switch-input" name="active" id="editActive" value="1">
-                <span class="switch-toggle-slider">
-                  <span class="switch-on"></span>
-                  <span class="switch-off"></span>
-                </span>
-                <span class="switch-label"  >تفعيل </span>
-              </label>
+    <!-- Add SubCategory Modal -->
+    <div class="modal fade" id="addSubCategoryModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="text-center mb-4">
+                        <h4 class="modal-title" id="userCrudModal"></h4>
+                        <p>اضف صنف جديدة للموقع</p>
+                    </div>
+                    <form id="addSubCategoryForm" class="row g-3" action="" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div>
+                            <label for="defaultFormControlInput" class="form-label"> الاسم (عربي)</label>
+                            <input type="text" name="nameAr" class="form-control  name ar" id="addCategoryNameAr"
+                                placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
+                            <span class="help-block text-danger"></span>
+                        </div>
+                        <div>
+                            <label for="defaultFormControlInput" class="form-label">الاسم (انجليزي)</label>
+                            <input type="text" name="nameEn" class="form-control  name en" id="addCategoryNameEn"
+                                placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
+                            <span class="help-block text-danger"></span>
+                        </div>
+                        <div class="col-12 my-4">
+                            <label class="switch">
+                                <input type="checkbox" class="switch-input active" name="active" id="addCategoryActive"
+                                    value="1">
+                                <span class="switch-toggle-slider">
+                                    <span class="switch-on"></span>
+                                    <span class="switch-off"></span>
+                                </span>
+                                <span class="switch-label">تفعيل </span>
+                            </label>
+                        </div>
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primary me-sm-3 me-1 mt-3 addSUbCategory">اضافة</button>
+                            <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal"
+                                aria-label="Close">Cancel</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="col-12 text-center">
-              <button type="submit" id="submit" class="btn btn-primary me-sm-3 me-1 mt-3 updateCategory">تعديل</button>
-              <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
-            </div>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
-  <!--/ Updat New Credit Card Modal -->
+    <!--/ Add SubCategory Modal -->
 
-   <!-- Active New Credit Card Modal -->
-   <div class="modal fade" id="activeCategoryModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
-      <div class="modal-content p-3 p-md-5">
-        <div class="modal-body">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          <div class="text-center mb-4">
-            <h4 class="modal-title" id="userCrudModal">هل انت متاكد انك تريد تغيير حاله الخدمة</h4>
-            <p>تغيير حالة الخدمة  </p>
-          </div>
-          <form id="activeCategory" class="row g-3" action="activeCategory" method="POST" enctype="multipart/form-data">
-            @csrf
-            <input type="hidden" id="deletcategoryId" >
-            <div class="col-12 text-center">
-              <button type="submit" id="submit" class="btn btn-primary me-sm-3 me-1 mt-3 activeCategory">تغيير الحالة</button>
-              <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+    <!-- Updat SubCategory Modal -->
+    <div class="modal fade" id="editSubCategoryModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="text-center mb-4">
+                        <h4 class="modal-title" id="userCrudModal"></h4>
+                        <p>تعديل الخدمة </p>
+                    </div>
+                    <form id="updateSubCategory" class="row g-3"  method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" id="editCategoryId">
+                        <div>
+                            <label for="defaultFormControlInput" class="form-label"> الاسم (عربي)</label>
+                            <input type="text" name="nameAr" class="form-control name ar" id="editCategoryNameAr"
+                                placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
+                            <span class="help-block text-danger"></span>
+                        </div>
+                        <div>
+                            <label for="defaultFormControlInput" class="form-label">الاسم (انجليزي)</label>
+                            <input type="text" name="nameEn" class="form-control" id="editCategoryNameEn"
+                                placeholder="اضف اسم الخدمة" aria-describedby="defaultFormControlHelp" />
+                            <span class="help-block text-danger"></span>
+                        </div>
+                        <div class="col-12 my-4">
+                            <label class="switch">
+                                <input type="checkbox" class="switch-input active" name="active"
+                                    id="editCategoryActive" value="1">
+                                <span class="switch-toggle-slider">
+                                    <span class="switch-on"></span>
+                                    <span class="switch-off"></span>
+                                </span>
+                                <span class="switch-label">تفعيل </span>
+                            </label>
+                        </div>
+                        <div class="col-12 text-center">
+                            <button type="submit"
+                                class="btn btn-primary me-sm-3 me-1 mt-3 updateSubCategory">تعديل</button>
+                            <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal"
+                                aria-label="Close">Cancel</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
-  <!--/ Updat New Credit Card Modal -->
+    <!--/ Updat SubCategory Modal -->
+
+    <!-- Active SubCategory Modal -->
+    <div class="modal fade activeSubCategoryModal" id="activeSubCategoryModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="text-center mb-4">
+                        <h4 class="modal-title" id="userCrudModal">هل انت متاكد انك تريد تغيير حاله الخدمة</h4>
+                        <p>تغيير حالة الخدمة </p>
+                    </div>
+                    <form id="activeSubCategory" class="row g-3" action="activeSubCategory" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" id="activeSubCategoryId" class="activeSubCategoryId" value="">
+                        <div class="col-12 text-center">
+                            <button type="submit" id="submit"
+                                class="btn btn-primary me-sm-3 me-1 mt-3 activeSubCategory">تغيير الحالة</button>
+                            <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal"
+                                aria-label="Close">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ Updat SubCategory Modal -->
+
+    <!-- Delete SubCategory Modal -->
+    <div class="modal fade deleteCategoryModal" id="deleteCategoryModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered1 modal-simple modal-add-new-cc">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="text-center mb-4">
+                        <h4 class="modal-title" id="userCrudModal">هل انت متاكد انك تريد حذف الخدمة</h4>
+                        <p>في حال الموافقه سوف يتم حذف الصنف بشكل نهائي ولن تستطيع التراجع </p>
+                    </div>
+                    <form id="deleteSubCategoryId" class="row g-3"  method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" id="deleteCategoryId" class="deleteCategoryId" value="">
+                        <div class="col-12 text-center">
+                            <button type="submit" id="submit"
+                                class="btn btn-primary me-sm-3 me-1 mt-3 deleteCategory">حذف الصنف</button>
+                            <button type="reset" class="btn btn-label-secondary btn-reset mt-3" data-bs-dismiss="modal"
+                                aria-label="Close">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ Delete SubCategory Modal -->
+@endsection
+@section('javascript')
+<script  src="{{URL::asset('js/subCategory.js')}}"></script>
 @endsection
